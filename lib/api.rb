ccb99af6849d61dfeb5545f1e85b3f95770b3ec5
Anthony Leon <acidknights89@gmail.com>
2020-10-26 02:42:51 +0000

    Added several methods, and saving progress.

--- lib/api.rb
+++ lib/api.rb
@@ -2,13 +2,23 @@ class API
 
   def self.fetch_drinks
     url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-    binding.pry
+    #binding.pry
     uri = URI(url)
     response = Net::HTTP.get(uri)
     #binding.pry
     hash = JSON.parse(response)
 
     array_of_drinks = hash["drinks"]
+
+    array_of_drinks.each do |drink_hash|
+      #binding.pry
+      drink = Drink.new
+      drink.name = drink_hash["strDrink"]
+      drink.instructions = drink_hash["strInstructions"]
+      drink.glass = drink_hash["strGlass"]
+      drink.german_instructions = drink_hash["strInstructionsDE"]
+    end
+    binding.pry 
   end
 
 end
